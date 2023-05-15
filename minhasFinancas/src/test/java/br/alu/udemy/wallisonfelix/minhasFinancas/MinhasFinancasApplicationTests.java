99b040ba298c5cf9e5aa344ccbbdc19f13d9b884
Wallison Felix <wallison.felix@gmail.com>
2023-05-15 19:15:03 -0300

    commit inicial do projeto

---
+++ minhasFinancas/src/test/java/br/alu/udemy/wallisonfelix/minhasFinancas/MinhasFinancasApplicationTests.java
@@ -0,0 +1,13 @@
+package br.alu.udemy.wallisonfelix.minhasFinancas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinhasFinancasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
